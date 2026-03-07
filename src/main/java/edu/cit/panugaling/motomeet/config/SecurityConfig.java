40effb3a377b109fc4c3de9715fc8adb97eff087
frnks1 <franklynjohn.panugaling@cit.edu>
2026-03-07 13:19:41 +0800

    IT342 Phase 1 – User Registration and Login Completed

---
+++ src/main/java/edu/cit/panugaling/motomeet/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package edu.cit.panugaling.motomeet.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/login", "/register", "/css/**", "/js/**", "/images/**").permitAll()
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
---
+++ src/test/java/edu/cit/panugaling/motomeet/MotomeetApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.panugaling.motomeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotomeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
